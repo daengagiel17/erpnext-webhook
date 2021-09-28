c290b74d70c8e16f70b36693c0716604af13e6bc
Daeng Agiel <agielnara17@gmail.com>
2021-09-28 16:04:30 +0700

    index

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('Run erpnext webhooks')
+})
+
 app.post('/mission-order', (req, res) => {
     console.log('Data mission order:' + JSON.stringify(req.body));
     res.send('Success')
