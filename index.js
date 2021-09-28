cc0f10669e3f5ae412c481798d0d5c1a3f890067
Daeng Agiel <agielnara17@gmail.com>
2021-09-28 15:59:39 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/mission-order', (req, res) => {
+    console.log('Data mission order:' + JSON.stringify(req.body));
+    res.send('Success')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
